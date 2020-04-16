e79efc532d97752172bc98f0827555499cd02532
MartinEichinger <martl.eichinger@gmail.com>
2020-04-16 11:23:08 +0200

    Added links to adress and image

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -2,18 +2,27 @@ import React from 'react';
 import './Business.css';
 
 class Business extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.hRef = `https://www.google.com/maps/place/${this.props.business.address} ${this.props.business.zipCode} ${this.props.business.city}`;
+    //this.hRef = `https://www.google.com/maps/place/${this.props.business.name} ${this.props.business.zipCode} ${this.props.business.city}`;
+  }
+
   render() {
     return (
       <div className="Business">
         <div className="image-container">
-          <img src={this.props.business.imageSrc} alt=''/>
+          <a href={this.props.business.url} target='_blank'>
+            <img src={this.props.business.imageSrc} alt=''/>
+          </a>
         </div>
         <h2>{this.props.business.name}</h2>
         <div className="Business-information">
           <div className="Business-address">
-            <p>{this.props.business.address}</p>
-            <p>{this.props.business.city}</p>
-            <p>{this.props.business.state + this.props.business.zipCode}</p>
+            <a href={this.hRef} target='_blank'>{this.props.business.address}</a>
+            <a href={this.hRef} target='_blank'>{this.props.business.city}</a>
+            <a href={this.hRef} target='_blank'>{this.props.business.state + this.props.business.zipCode}</a>
           </div>
           <div className="Business-reviews">
             <h3>{this.props.business.category}</h3>
