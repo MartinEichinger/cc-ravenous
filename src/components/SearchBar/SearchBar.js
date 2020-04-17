58328edcb50fca14441e3baab9810764f1d1387d
MartinEichinger <martl.eichinger@gmail.com>
2020-04-17 12:07:19 +0200

    Update

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -24,6 +24,7 @@ class SearchBar extends React.Component {
     this.handleLocationChange = this.handleLocationChange.bind(this);
     this.handleSearch = this.handleSearch.bind(this);
     this.handleCombined = this.handleCombined.bind(this);
+    this.keyPressed = this.keyPressed.bind(this);
     //this.handleSortByChange = this.handleSortByChange.bind(this);
   }
   getSortByClass(sortByOption) {
@@ -51,9 +52,7 @@ class SearchBar extends React.Component {
   }
 
   handleSearch(event) {
-    //console.log('handleSearch');
-    //console.log(this.state.term, this.state.location, this.state.sortBy);
-    //console.log(event);
+    console.log('handleSearch');
     if (this.state.term !== '' && this.state.location !== ''){
       this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy);
     }
@@ -61,36 +60,39 @@ class SearchBar extends React.Component {
   }
 
   handleCombined(event, sortByOptionValue) {
-    //console.log('handleCombined');
-    //console.log('hC: ', sortByOptionValue);
-    //console.log('hC: ', event);
+    console.log('handleCombined');
     this.handleSortByChange(sortByOptionValue);
     this.handleSearch(event);
+  }
+
+  keyPressed(event) {
+    console.log('Key Pressed');
+    if (event.key === "Enter") {
+      this.handleSearch(event);
+    }
   }
 
   renderSortByOptions() {
     return Object.keys(this.sortByOptions).map(sortByOption => {
       let sortByOptionValue = this.sortByOptions[sortByOption];
       //return <li onClick={this.handleSortByChange.bind(this, sortByOptionValue)} className={this.getSortByClass(sortByOptionValue)} key={sortByOptionValue}>{sortByOption} </li>;
-      //return <li onClick={() => {this.handleSortByChange(sortByOptionValue);}} className={this.getSortByClass(sortByOptionValue)} key={sortByOptionValue}>{sortByOption} </li>;
       return <li onClick={(event) => {this.handleCombined(event, sortByOptionValue)}} className={this.getSortByClass(sortByOptionValue)} key={sortByOptionValue}>{sortByOption} </li>;
-      //return <li onClick={this.handleCombined(event)} className={this.getSortByClass(this.sortByOptionValue)} key={this.sortByOptionValue}>{sortByOption} </li>;
     });
   }
 
   render() {
     return (
       <div className="SearchBar">
-        <div className="SearchBar-sort-options">
+        <div className="SearchBar-sort-options" onKeyPress={this.keyPressed}>
           <ul>
             {this.renderSortByOptions()}
           </ul>
         </div>
-        <div className="SearchBar-fields">
+        <div className="SearchBar-fields" onKeyPress={this.keyPressed}>
           <input placeholder="Search Businesses" onChange={this.handleTermChange} />
           <input placeholder="Where?" onChange={this.handleLocationChange} />
         </div>
-        <div className="SearchBar-submit" onClick={this.handleSearch}>
+        <div className="SearchBar-submit" onClick={this.handleSearch} onKeyPress={this.keyPressed}>
           <a href="www.#.com">Let's Go</a>
         </div>
       </div>
